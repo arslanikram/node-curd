a2d355a481adea87527741ce86f320b3a1649205
arslanikram <arslanikramarain@gmail.com>
2021-06-18 12:06:41 +0500

    db added

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # node-curd
+
+#### Step 1 
+###### * Open gitbas or command prompt & running the following command
+`git clone https://arslan_ikram@bitbucket.org/tossdownInc/node-curd.git`
+
+#### Step 2 
+<p>Create a database using MySQL named 'restaurants_db' and import the "restaurants_db.sql" file in this database.</p>
+<p>If you want to change database name change it from "Config -> database.js" file.</p>
+
+#### Step 3
+##### Run the following two commands
+`npm install`
+<p> & after that run</p>
+`npm start` 
+<br><br>
+<p>That's all your Node MVC CURD is running now on <br> 'http://localhost:5000' </p>
+
+#### Step 4 
+<p>Use the following link to import APIs collection in your Postman and Test the CURD APIs</p>
+
+[APIs Collection](https://www.getpostman.com/collections/05974e89750a66cdfa48)
---
+++ restaurants_db.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Ultimate v8.61 
+MySQL - 5.5.5-10.4.17-MariaDB : Database - restaurants_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+/*Table structure for table `restaurants` */
+
+DROP TABLE IF EXISTS `restaurants`;
+
+CREATE TABLE `restaurants` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `r_name` varchar(255) DEFAULT NULL,
+  `r_branch_code` varchar(50) DEFAULT NULL,
+  `r_address` varchar(255) DEFAULT NULL,
+  `r_city` varchar(50) DEFAULT NULL,
+  `r_state` varchar(50) DEFAULT NULL,
+  `r_country` varchar(50) DEFAULT NULL,
+  `r_created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
